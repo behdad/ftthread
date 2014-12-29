05a1d21be47a71c8009464655cabcde6d35ddf5d
Behdad Esfahbod <behdad@behdad.org>
2014-12-29 12:25:01 -0800

    Move face creation into thread and protect with mutex
    Also recreate face every once in a while.
    
    Runs happily agasint FreeType master so far.  Next, we will add
    rasterization.

--- ftthread.c
+++ ftthread.c
@@ -1,29 +1,56 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
+#include <assert.h>
 
+#include <pthread.h>
 #include <ft2build.h>
 #include FT_FREETYPE_H
 
 #define DIE(msg) do { fprintf (stderr, msg "\n"); abort (); } while (0)
 
+#define MAX_NUM_THREADS 4096
+
 int num_iters = 100;
 int ppem = 100;
-FT_Library ft_library;
 const char *font_file = NULL;
+
+pthread_mutex_t lock;
+FT_Library ft_library;
 
 static void *
 draw_thread (void *arg)
 {
   int i;
-  FT_Face ft_face = (FT_Face) arg;
+  FT_Face face = NULL;
+  arg = arg;
 
   for (i = 0; i < num_iters; i++)
   {
-    FT_Load_Glyph (ft_face, 50, 0);
-//    printf("%d\n", i);
+    if (!face)
+    {
+      pthread_mutex_lock (&lock);
+      if (FT_New_Face (ft_library, font_file, 0, &face))
+	DIE ("Failed creating face.");
+      pthread_mutex_unlock (&lock);
+      if (FT_Set_Char_Size (face, ppem, ppem, 0, 0))
+	DIE ("FT_Set_Char_Size failed.");
+    }
+
+    FT_Load_Glyph (face, 50, 0);
+
+    if ((i & 63) == 0)
+    {
+      pthread_mutex_lock (&lock);
+      FT_Done_Face (face);
+      if (FT_New_Face (ft_library, font_file, 0, &face))
+	DIE ("Failed recreating face.");
+      pthread_mutex_unlock (&lock);
+    }
   }
+
+  if (face)
+    FT_Done_Face (face);
 
   return NULL;
 }
@@ -32,6 +59,8 @@ int
 main (int argc, char **argv)
 {
   int num_threads = 100;
+  pthread_t threads[MAX_NUM_THREADS];
+  int i;
 
   if (argc < 2)
   {
@@ -49,19 +78,14 @@ main (int argc, char **argv)
   if (argc > 4)
     ppem = atoi (argv[4]);
 
-  pthread_t threads[num_threads];
-  FT_Face thread_face[num_threads];
-  int i;
+  assert (num_threads <= MAX_NUM_THREADS);
 
+  pthread_mutex_init (&lock, NULL);
   FT_Init_FreeType (&ft_library);
 
   for (i = 0; i < num_threads; i++)
   {
-    if (FT_New_Face (ft_library, font_file, 0, &thread_face[i]))
-      DIE ("Failed opening face.");
-    if (FT_Set_Char_Size (thread_face[i], ppem, ppem, 0, 0))
-      DIE ("FT_Set_Char_Size failed.");
-    if (pthread_create (&threads[i], NULL, draw_thread, thread_face[i]) != 0)
+    if (pthread_create (&threads[i], NULL, draw_thread, NULL) != 0)
       DIE ("pthread_create() failed.");
   }
 
@@ -69,10 +93,10 @@ main (int argc, char **argv)
   {
     if (pthread_join (threads[i], NULL) != 0)
       DIE ("pthread_join() failed.");
-    FT_Done_Face ((FT_Face) thread_face[i]);
   }
 
   FT_Done_FreeType (ft_library);
+  pthread_mutex_destroy (&lock);
 
   return 0;
 }
